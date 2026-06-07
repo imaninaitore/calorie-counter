5b37e0a968b85825f19f7b12ca3d4e360d77cd1c
imaninaitore <imaninaitore1726@gmail.com>
2026-06-07 15:30:56 +0300

    comment out fetched data and calculation code

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+//create variable before using
+const totalCalories = 
+document.getElementById("totalCalories");
+
+const foodContainer = 
+document.getElementById("foodContainer");
+//fetching data
 const url = "https://dummyjson.com/recipes";
 
 fetch(url)
@@ -15,26 +23,44 @@ fetch(url)
     });
 
 //grabbing the food container
-const foodContainer = 
-document.getElementById("foodContainer");
+
 
 data.recipes.forEach(food => {
 //creating div 
-const div = document.createElement("div")
+const div = document.createElement("div");
 //creating checkbox
 const checkbox =document.createElement("input");checkbox.type = "checkbox";
+
+//calculation
+//find calorie number
+let total = 0;
+
+//listen for check
+checkbox.addEventListener("change", () => {
+if (checkbox.checked) {
+    total = total + food.caloriesPerServing;
+} else {
+    total = total - food.caloriesPerServing;
+}
+
+totalCalories.textContent = total;
+});
+
+
 //creating span
 const span = document.createElement("span");
 
 span.textContent = `${food.name} - ${food.caloriesPerServing} cal`;
 
-foodContainer.appendChild(checkbox);    
-foodContainer.appendChild(span);    
 foodContainer.appendChild(div);
+div.appendChild(checkbox);    
+div.appendChild(span);    
 });
 
 })
 
 .catch(error =>
 console.error("Error fetching data:", error));
+
+*/
 
