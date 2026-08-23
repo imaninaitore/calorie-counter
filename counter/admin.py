8c2d6b337250554a51fc414747e51f72ed13f78f
imaninaitore <imaninaitore1726@gmail.com>
2026-08-23 21:38:07 +0300

    register food model in the admin

--- counter/admin.py
+++ counter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Import the Food model
+from .models import Food
+
+
+# Register the Food model with Django's admin site
+admin.site.register(Food)
