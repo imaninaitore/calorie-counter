6236032bd56a3a626bc5ef80d23de5893cebf02f
imaninaitore <imaninaitore1726@gmail.com>
2026-08-23 22:20:17 +0300

    create add and delete views

--- counter/views.py
+++ counter/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
 # Import our Food model
 from .models import Food
+# import the food form
+from .forms import FoodForm
 
 # Create the homepage view
 def home(request):
@@ -19,5 +21,64 @@ def home(request):
         {
             'foods': foods,
             'total_calories': total_calories,
+        }
+    )
+
+
+# Display the form for adding a new food
+def add_food(request):
+
+    # Check if the user submitted the form
+    if request.method == 'POST':
+
+        # Create a form using the submitted data
+        form = FoodForm(request.POST)
+
+        # Check if the submitted form is valid
+        if form.is_valid():
+
+            # Save the new food to the database
+            form.save()
+
+            # Return the user to the homepage
+            return redirect('home')
+
+    # If the user has not submitted the form
+    else:
+
+        # Create an empty food form
+        form = FoodForm()
+
+    # Display the add food page
+    return render(
+        request,
+        'add_food.html',
+        {
+            'form': form,
+        }
+    )
+
+
+# Delete a food item
+def delete_food(request, food_id):
+
+    # Find the food item using its ID
+    food = get_object_or_404(Food, id=food_id)
+
+    # Check if the user submitted the delete confirmation
+    if request.method == 'POST':
+
+        # Delete the food from the database
+        food.delete()
+
+        # Return the user to the homepage
+        return redirect('home')
+
+    # Display the delete confirmation page
+    return render(
+        request,
+        'confirm_delete.html',
+        {
+            'food': food,
         }
     )
