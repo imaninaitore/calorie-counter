79ec142aba874b6efd957ea4a4d3d3368ea4959b
imaninaitore <imaninaitore1726@gmail.com>
2026-08-23 22:01:56 +0300

    import food model and create the home view

--- counter/views.py
+++ counter/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Import our Food model
+from .models import Food
+
+# Create the homepage view
+def home(request):
+
+    # Get all food items from the database
+    foods = Food.objects.all()
+
+    # Calculate the total calories of all food items
+    total_calories = sum(food.calories for food in foods)
+
+    # Send the food and total calories to the HTML template
+    return render(
+        request,
+        'counter/home.html',
+        {
+            'foods': foods,
+            'total_calories': total_calories,
+        }
+    )
